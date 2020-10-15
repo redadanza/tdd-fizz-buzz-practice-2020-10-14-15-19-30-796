ec0f0113c10d3d62c31e260220df5bb32bef2de2
RAYMOND ALFRED ADANZA (IM-MIS-ISD-OOCLL/MNL) <ADANZRA@OOCL.COM>
2020-10-15 14:07:23 +0800

    [FizzBuzz][Raymond]Test: implement the multiple of 5 case with input 5 and output Buzz

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -30,5 +30,17 @@ public class FizzBuzzTest {
         //then
         assertEquals("Fizz", actual);
     }
+    @Test
+    void should_return_Buzz_when_count_off_given_order_number_5(){
+        //given
+        int orderNum = 5;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        //when
+        String actual = fizzBuzz.countOff(orderNum);
+
+        //then
+        assertEquals("Buzz", actual);
+    }
 
 }
