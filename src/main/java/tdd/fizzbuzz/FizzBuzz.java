7f771a645fdbd9ed9dd824ba6add487363c394e0
RAYMOND ALFRED ADANZA (IM-MIS-ISD-OOCLL/MNL) <ADANZRA@OOCL.COM>
2020-10-15 14:03:04 +0800

    [FizzBuzz][Raymond]Feature: should return Fizz when count off given order number 3

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,7 +2,13 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
 
+    public static final String FIZZ = "Fizz";
+
     public String countOff(int orderNumber){
+        if(orderNumber % 3 == 0)
+        {
+            return  FIZZ;
+        }
 
         return String.valueOf(orderNumber);
     }
