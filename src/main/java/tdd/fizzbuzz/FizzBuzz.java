c22ca699d41f773112d8b9d4671e8dca910541b1
RAYMOND ALFRED ADANZA (IM-MIS-ISD-OOCLL/MNL) <ADANZRA@OOCL.COM>
2020-10-15 14:10:50 +0800

    [FizzBuzz][Raymond]Feature: implement the multiple of 5 case with input 5 and output Buzz

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -3,11 +3,16 @@ package tdd.fizzbuzz;
 public class FizzBuzz {
 
     public static final String FIZZ = "Fizz";
+    public static final String BUZZ = "Buzz";
 
     public String countOff(int orderNumber){
         if(orderNumber % 3 == 0)
         {
             return  FIZZ;
+        }
+        if(orderNumber % 5 == 0)
+        {
+            return BUZZ;
         }
 
         return String.valueOf(orderNumber);
